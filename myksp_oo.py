623fa77757d07ea1d824d5b4371885dd064693f7
Albert Vonpupp <vonpupp@gmail.com>
2013-10-19 10:54:35 -0300

    Added n bins solving

--- myksp_oo.py
+++ myksp_oo.py
@@ -12,6 +12,7 @@ def jformat(json_data):
 
 items = []
 constraints = None
+hosts = 10
 
 def gen_vms():
     global items
@@ -38,25 +39,81 @@ def gen_costraints(constraint_list):
     global constraints
     constraints = lambda values: (add_constraints(values, constraint_list))
 
-def solve1():
+def get_item_values(index):
+    result = get_item_index(index)
+    if result is not -1:
+        result = items[result]
+    else:
+        result = None
+    return result
+	  
+def get_item_index(id):
+    result = -1
+    i = 0
+    found = False
+    while i < len(items) and not found:
+        item = items[i]
+        j = item['name'] #int(item['name'].split()[1])
+#        print('  get_item_index={} j={}'.format(id, j))
+        found = j == id
+        if found:
+            result = i
+        i += 1
+    return result
+  
+#def get_item_id(value):
+#    result = -1
+#    item = items[value]
+#    result = int(item['name'].split()[1])
+#    return result
+
+def items_remove(remove_list):
+    global items
+    for to_delete in remove_list:
+        i = get_item_index(to_delete)
+        if i is not -1:
+            del items[i]
+
+def solve_host():
     global items
     global constraints
-    print constraints
-    p = KSP('weight', items, constraints = constraints)
-    return p.solve('glpk', iprint = 0)
-    
+#    print constraints
+    p = KSP('weight', list(items), constraints = constraints)
+    result = p.solve('glpk', iprint = 0)
+    #print('INDEX: {}'.format(map(get_item_index, [12, 4])))
+    return result
+
+def solve_hosts():
+    for host in range(hosts):
+        vms = solve_host()
+        if vms is not None:
+            print_results(vms)
+            #sorted_vms = sort(vms.xf)
+#            print('solve_hosts: {}'.format(map(get_item_index, vms.xf)))
+            print_items('vms', vms.xf)
+            items_remove(vms.xf)
+#        print('items: {}'.format(items))
+
+def print_items(caption, items_list):
+    to_print = []
+    for item in items_list:
+        i = int(item.split()[1])
+        to_print += [i]
+    print('{}: {}'.format(caption, to_print))
+
 def print_results(r):
-    print(r.xf)
+    #print(r.xf)
     cpu = mem = disk = net = 0
     weight = 0
     for item in r.xf:
         i = int(item.split()[1])
-        print('{}, values: {}'.format(item, items[i]))
-        cpu += items[i]['cpu']
-        mem += items[i]['mem']
-        disk += items[i]['disk']
-        net += items[i]['net']
-        weight += items[i]['weight']
+        v = get_item_values(item)
+        print('{}, values: ({})'.format(item, v))
+        cpu += v['cpu']
+        mem += v['mem']
+        disk += v['disk']
+        net += v['net']
+        weight += v['weight']
 #        print('item: {}: '.format(i))
 #        print('item: {}: {}'.format(i, jformat(items[i])))
 
@@ -74,5 +131,6 @@ gen_vms()
 #gen_costraints(['net'])
 #gen_costraints(['net', 'cpu'])
 gen_costraints(['cpu', 'mem', 'disk', 'net'])
-r = solve1()
-print_results(r)
+#r = solve_host()
+solve_hosts()
+#print_results(r)
