40641935d5fb21ff8c3fa3c40c8272f556d065fd
Albert Vonpupp <vonpupp@gmail.com>
2013-10-21 08:25:00 -0200

    Modified my_evaluator, now seems to work as the original example

--- inspyred/myknapsack_acs_example.py
+++ inspyred/myknapsack_acs_example.py
@@ -9,8 +9,18 @@ def my_evaluator(candidate, args):
     # Check the constraints.
     print('candidate: {}'.format(candidate))
     print('args: {}'.format(args))
-    #return 100
-    return True
+    values = list(candidate)
+#    from pudb import set_trace; set_trace()
+#    print('type(values): {}'.format(type(values)))
+    values_sum = sum([trail.value for trail in candidate])
+    print('values_sum: {}'.format(values_sum))
+    return values_sum
+#    print('values: {}'.format(values))
+#    total_weight = sum([value for id, value in candidate])
+#    return True
+#    print('total_weight: {}'.format(total_weight))
+#    return total_weight
+#    return 100
     #constraint_bounds = [-2, -4]
     #constraint_coefficients = [[-3, -3, 1, 2, 3], [-5, -3, -2, -1, 1]]
     #constraints = [sum([c * v for c, v in zip(coeff, candidate)]) for coeff in constraint_coefficients]
@@ -19,14 +29,14 @@ def my_evaluator(candidate, args):
     #for constraint, bound in zip(constraints, constraint_bounds):
         #if constraint > bound:
             #failed_constraints += 1
-            
-    ## Now calculate the fitness. Depending on the computational time, 
+
+    ## Now calculate the fitness. Depending on the computational time,
     ## failed constraints may cause us to just skip this part.
     #coefficients = [8, 2, 4, 7, 5]
     #fitness = 0
     #for c, v in zip(coefficients, candidate):
         #fitness += c * v
-    
+
     ## Now, punish the candidate for any failed constraints. For the current
     ## problem, the largest (i.e., worst) value for any candidate is 26, so
     ## we'll make sure that our constraint penalty is larger than that.
@@ -34,31 +44,31 @@ def my_evaluator(candidate, args):
     #return failed_constraints * 100 + fitness
 
 
-def main(prng=None, display=False):    
+def main(prng=None, display=False):
     if prng is None:
         prng = Random()
-        prng.seed(time()) 
+        prng.seed(time())
 
-    items = [(7,369), (10,346), (11,322), (10,347), (12,348), (13,383), 
-             (8,347), (11,364), (8,340), (8,324), (13,365), (12,314), 
-             (13,306), (13,394), (7,326), (11,310), (9,400), (13,339), 
-             (5,381), (14,353), (6,383), (9,317), (6,349), (11,396), 
-             (14,353), (9,322), (5,329), (5,386), (5,382), (4,369), 
-             (6,304), (10,392), (8,390), (8,307), (10,318), (13,359), 
+    items = [(7,369), (10,346), (11,322), (10,347), (12,348), (13,383),
+             (8,347), (11,364), (8,340), (8,324), (13,365), (12,314),
+             (13,306), (13,394), (7,326), (11,310), (9,400), (13,339),
+             (5,381), (14,353), (6,383), (9,317), (6,349), (11,396),
+             (14,353), (9,322), (5,329), (5,386), (5,382), (4,369),
+             (6,304), (10,392), (8,390), (8,307), (10,318), (13,359),
              (9,378), (8,376), (11,330), (9,331)]
 
     problem = inspyred.benchmarks.Knapsack(15, items, duplicates=False)
     ac = inspyred.swarm.ACS(prng, problem.components)
     ac.terminator = inspyred.ec.terminators.generation_termination
     final_pop = ac.evolve(problem.constructor,
-#                          problem.evaluator, 
+#                          problem.evaluator,
                           evaluator=my_evaluator,
                           maximize=problem.maximize,
 #                          maximize=False,
-                          pop_size=50, 
+                          pop_size=50,
                           max_generations=50,
                          )
-    
+
 #final_pop = optimizer.evolve(generator=my_generator,
 #                             evaluator=my_evaluator,
 #                             pop_size=2,
@@ -72,10 +82,10 @@ def main(prng=None, display=False):
 
     if display:
         best = max(ac.archive)
-        print('Best Solution: {0}: {1}'.format(str(best.candidate), 
+        print('Best Solution: {0}: {1}'.format(str(best.candidate),
                                                best.fitness))
 	# Best Solution: [(5, 386), (6, 383), (4, 369)]: 1138
     return ac
-            
+
 if __name__ == '__main__':
     main(display=True)
