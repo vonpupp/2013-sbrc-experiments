8044b6e5b249a5c9ee772c0505cf4c3673d30bde
Albert Vonpupp <vonpupp@gmail.com>
2013-10-21 00:15:25 -0200

    Added a custom ec evaluator

--- inspyred/myknapsack_acs_example.py
+++ inspyred/myknapsack_acs_example.py
@@ -7,27 +7,31 @@ import inspyred
 @inspyred.ec.evaluators.evaluator
 def my_evaluator(candidate, args):
     # Check the constraints.
-    constraint_bounds = [-2, -4]
-    constraint_coefficients = [[-3, -3, 1, 2, 3], [-5, -3, -2, -1, 1]]
-    constraints = [sum([c * v for c, v in zip(coeff, candidate)]) for coeff in constraint_coefficients]
-    print constraints
-    failed_constraints = 0
-    for constraint, bound in zip(constraints, constraint_bounds):
-        if constraint > bound:
-            failed_constraints += 1
+    print('candidate: {}'.format(candidate))
+    print('args: {}'.format(args))
+    #return 100
+    return True
+    #constraint_bounds = [-2, -4]
+    #constraint_coefficients = [[-3, -3, 1, 2, 3], [-5, -3, -2, -1, 1]]
+    #constraints = [sum([c * v for c, v in zip(coeff, candidate)]) for coeff in constraint_coefficients]
+    #print constraints
+    #failed_constraints = 0
+    #for constraint, bound in zip(constraints, constraint_bounds):
+        #if constraint > bound:
+            #failed_constraints += 1
             
-    # Now calculate the fitness. Depending on the computational time, 
-    # failed constraints may cause us to just skip this part.
-    coefficients = [8, 2, 4, 7, 5]
-    fitness = 0
-    for c, v in zip(coefficients, candidate):
-        fitness += c * v
+    ## Now calculate the fitness. Depending on the computational time, 
+    ## failed constraints may cause us to just skip this part.
+    #coefficients = [8, 2, 4, 7, 5]
+    #fitness = 0
+    #for c, v in zip(coefficients, candidate):
+        #fitness += c * v
     
-    # Now, punish the candidate for any failed constraints. For the current
-    # problem, the largest (i.e., worst) value for any candidate is 26, so
-    # we'll make sure that our constraint penalty is larger than that.
-    # (We'll make it an order of magnitude larger at 100.)
-    return failed_constraints * 100 + fitness
+    ## Now, punish the candidate for any failed constraints. For the current
+    ## problem, the largest (i.e., worst) value for any candidate is 26, so
+    ## we'll make sure that our constraint penalty is larger than that.
+    ## (We'll make it an order of magnitude larger at 100.)
+    #return failed_constraints * 100 + fitness
 
 
 def main(prng=None, display=False):    
@@ -46,14 +50,31 @@ def main(prng=None, display=False):
     problem = inspyred.benchmarks.Knapsack(15, items, duplicates=False)
     ac = inspyred.swarm.ACS(prng, problem.components)
     ac.terminator = inspyred.ec.terminators.generation_termination
-    final_pop = ac.evolve(problem.constructor, problem.evaluator, 
-                          maximize=problem.maximize, pop_size=50, 
-                          max_generations=50)
+    final_pop = ac.evolve(problem.constructor,
+#                          problem.evaluator, 
+                          evaluator=my_evaluator,
+                          maximize=problem.maximize,
+#                          maximize=False,
+                          pop_size=50, 
+                          max_generations=50,
+                         )
+    
+#final_pop = optimizer.evolve(generator=my_generator,
+#                             evaluator=my_evaluator,
+#                             pop_size=2,
+#                             maximize=False,
+#                             bounder=inspyred.ec.DiscreteBounder([0, 1]),
+#                             max_evaluations=10,
+#                             tournament_size=2,
+#                             mutation_rate=0.1,
+#                             num_elites=1,
+#                             num_inputs=5)
 
     if display:
         best = max(ac.archive)
         print('Best Solution: {0}: {1}'.format(str(best.candidate), 
                                                best.fitness))
+	# Best Solution: [(5, 386), (6, 383), (4, 369)]: 1138
     return ac
             
 if __name__ == '__main__':
