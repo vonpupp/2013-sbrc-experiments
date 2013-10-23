e2e8a2be320c8f77d67a5bc6bb822510564e80f3
Albert Vonpupp <vonpupp@gmail.com>
2013-10-22 22:49:35 -0200

    Added power accounting

--- myksp_oo.py
+++ myksp_oo.py
@@ -11,11 +11,13 @@ def jformat(json_data):
                       indent=4, separators=(',', ': '))
 
 items = []
+vms_list = []
 constraints = None
 hosts = 10
 
+
+
 def gen_vms():
-    global items
     tg = tracegen.TraceGenerator()
     trace = tg.gen_trace()
     items = [{
@@ -28,6 +30,7 @@ def gen_vms():
                   'net': t[3],
                   'n': 1
              } for i,t in islice(enumerate(trace), 200)]
+    return items
 
 def add_constraint(values, constraint):
     return values[constraint] < 99
@@ -39,10 +42,10 @@ def gen_costraints(constraint_list):
     global constraints
     constraints = lambda values: (add_constraints(values, constraint_list))
 
-def get_item_values(index):
-    result = get_item_index(index)
+def get_item_values(id):
+    result = get_item_index(id)
     if result is not -1:
-        result = items[result]
+        result = vms_list[result]
     else:
         result = None
     return result
@@ -51,8 +54,8 @@ def get_item_index(id):
     result = -1
     i = 0
     found = False
-    while i < len(items) and not found:
-        item = items[i]
+    while i < len(vms_list) and not found:
+        item = vms_list[i]
         j = item['name'] #int(item['name'].split()[1])
 #        print('  get_item_index={} j={}'.format(id, j))
         found = j == id
@@ -87,8 +90,10 @@ def solve_hosts():
     placement = []
     for host in range(hosts):
         vms = solve_host()
-        print('HOST: {} ({})'.format(host, items_str('VMs', vms.xf)))
-        #placement[host] = 0
+        ids = get_openopt_vms(vms.xf)
+        print('HOST: {} ({})'.format(host, ids))
+        #print('HOST: {} ({})'.format(host, items_str('VMs', vms.xf)))
+        placement.append(ids)
         if vms is not None:
             print_results(vms)
             #sorted_vms = sort(vms.xf)
@@ -97,21 +102,20 @@ def solve_hosts():
             items_remove(vms.xf)
 #        print('items: {}'.format(items))
     return placement
-   
-def items_str(caption, items_list):
-    to_print = []
+
+def get_openopt_vms(items_list):
+    result = []
     for item in items_list:
-        i = int(item.split()[1])
-        to_print += [i]
-    return '{}: {}'.format(caption, to_print)
-
-
+        # i = int(item.split()[1])
+        result += [item]
+    return result
+   
 def print_results(r):
     #print(r.xf)
     cpu = mem = disk = net = 0
     weight = 0
     for item in r.xf:
-        i = int(item.split()[1])
+        #i = int(item.split()[1])
         v = get_item_values(item)
         print('  {}, values: ({})'.format(item, v))
         cpu += v['cpu']
@@ -130,9 +134,42 @@ def print_results(r):
     print('  net: {}'.format(net))
     print('  weight: {}'.format(weight))
 
+def cpu_power(cpu):
+    # P(cpu) = P_idle + (P_busy - P_idle) x cpu
+    p_idle = 114.0
+    p_busy = 250.0
+    result = p_idle + (p_busy - p_idle) * cpu/100
+    return result
+    
+def get_cpu(host):
+    result = 0
+    for vm_id in host:
+        #result += calculate_vm_power(vm)
+        print('  vm_id: {}'.format(vm_id))
+        vm_idx = get_item_index(vm_id)
+        print('  vm_idx: {}'.format(vm_idx))
+        vm = vms_list[vm_idx]
+        print('  vm: {}'.format(vm))
+        cpu = vm['cpu']
+        print('  cpu: {}'.format(vm['cpu']))
+        result += cpu
+        print('  result: {}'.format(result))
+    return result
+
+def calculate_placement_power(placement):
+    result = 0.0
+    for host in placement:
+        #result += calculate_host_power(host)
+        host_power = cpu_power(get_cpu(host))
+        result += host_power
+        print('HOST POWER: {}'.format(host_power))
+    return result
 
 def my_multi_bpp_place():
-    gen_vms()
+    global items
+    global vms_list
+    items = gen_vms()
+    vms_list = gen_vms()
 #    gen_costraints(['cpu'])
 #    gen_costraints(['net'])
 #    gen_costraints(['net', 'cpu'])
@@ -140,6 +177,8 @@ def my_multi_bpp_place():
 #    r = solve_host()
     placement = solve_hosts()
     print placement
+    power = calculate_placement_power(placement)
+    print('TOTAL POWER: {} WATTS'.format(power))
 #    print_results(r)
 
 
