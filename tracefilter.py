1c7e64be3c2b2e1de5e18f3148c531f132130281
Albert Vonpupp <vonpupp@gmail.com>
2013-11-06 19:15:46 -0200

    Filtered data to get trace scenarios

--- tracefilter.py
+++ tracefilter.py
@@ -29,10 +29,26 @@ import distsim.model.tracefilter as tf
 if __name__ == "__main__":
     t = tf.TraceFilter('traces.csv')
 #    t.by_mean()
-    data = tf.filter_equals(t.data, 'var', 1000)
-    data = tf.filter_range(t.data, 'var', 1000, 1500)
-    t.filter_rows_by_range('var', 1000, 1500)
-    t.csv_write()
+#    data = tf.filter_equals(t.data, 'var', 1000)
+#    data = tf.filter_range(t.data, 'var', 1000, 1500)
+
+#    data = tf.filter_range(t.data, 'std', 15, 16)
+#    data = tf.filter_equals(data, 'mean', 25)
+#    data = tf.filter_equals(data, 'mean', 35)
+#    data = tf.filter_equals(data, 'mean', 45)
+
+#    data = tf.filter_range(t.data, 'std', 30, 31)
+#    data = tf.filter_equals(data, 'mean', 25)
+#    data = tf.filter_equals(data, 'mean', 35)
+#    data = tf.filter_equals(data, 'mean', 45)
+
+    data = tf.filter_range(t.data, 'std', 35, 45)
+#    data = tf.filter_equals(data, 'mean', 25)
+#    data = tf.filter_equals(data, 'mean', 35)
+    data = tf.filter_equals(data, 'mean', 45)
+    print data
+#    t.filter_rows_by_range('var', 1000, 1500)
+#    t.csv_write()
 #    print data
     print t.filtered_data
     print('done')
