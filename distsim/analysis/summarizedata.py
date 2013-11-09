a2f5006ee173cf7c48bcf283b016097101f1333b
Albert Vonpupp <vonpupp@gmail.com>
2013-11-09 12:26:30 -0200

    Added 95% confidence interval

--- distsim/analysis/summarizedata.py
+++ distsim/analysis/summarizedata.py
@@ -24,18 +24,17 @@ __author__  = "Albert De La Fuente"
 
 #import collections
 import numpy as np
+import scipy as sp
+import scipy.stats
 #import scipy.stats as stats
 import csv
 import glob
 import os
 
-
 class SummarizeData():
     def __init__(self, working_dir):
         self.working_dir = working_dir
-#        self.simulation_counter = 0
         self.data = []
-#        self.data[self.simulation_counter] = []
         self.summary_list = {}
 
     def remap_data(self):
@@ -46,8 +45,6 @@ class SummarizeData():
             d = {}
             for id_repetition, repetition in enumerate(self.data):
                 data = repetition[scenario]
-#                for id_scenario, scenario in enumerate(repetition):
-                #d = dict(l[id_scenario])
                 for attribute in data.keys():
                     value = data[attribute]
                     try:
@@ -72,18 +69,25 @@ class SummarizeData():
     def worst_best_medium(self, scenario, column, bselector, wselector, mselector):
         worst_case = self.worst_case[len(self.worst_case)-1]
         worst_case[column] = self.map_column(scenario, column, wselector)
-        #self.worst_case += worst_case
+        #test = scipy.stats.norm.interval(0.95, loc=mean, scale=std)
+        try:
+            worst_case[column + '-95'] = np.percentile(scenario[column], 95)
+        except:
+            worst_case[column + '-95'] = 0
         
         best_case = self.best_case[len(self.best_case)-1]
         best_case[column] = self.map_column(scenario, column, bselector)
-        #self.best_case += best_case
+        try:
+            best_case[column + '-95'] = np.percentile(scenario[column], 95)
+        except:
+            best_case[column + '-95'] = 0
         
-        medium_case = self.medium_case[len(self.medium_case)-1]
-        medium_case[column] = self.map_column(scenario, column, mselector)
-        #self.medium_case += medium_case
-        
-        #self.best_case.append()[c] = self.map_column(scenario, column, bselector)
-        #self.medium_case.append()[c] = self.map_column(scenario, column, mselector)
+        average_case = self.average_case[len(self.average_case)-1]
+        average_case[column] = self.map_column(scenario, column, mselector)
+        try:
+            average_case[column + '-95'] = np.percentile(scenario[column], 95)
+        except:
+            average_case[column + '-95'] = 0
     
     def first_item(self, l):
         return l[0]
@@ -91,11 +95,11 @@ class SummarizeData():
     def summarize_data(self):
         self.worst_case = []
         self.best_case = []
-        self.medium_case = []
+        self.average_case = []
         for scenario in self.summary_list:
             self.worst_case.append({})
             self.best_case.append({})
-            self.medium_case.append({})
+            self.average_case.append({})
             self.worst_best_medium(scenario, '#PM', min, max, np.mean)
             self.worst_best_medium(scenario, '#VM', max, min, np.mean)
             self.worst_best_medium(scenario, '#PM-U', max, min, np.mean)
@@ -109,51 +113,48 @@ class SummarizeData():
 
     def load_file(self, fname):
         self.fname = fname
-        self.file_in = open(fname)
-        self.reader = csv.DictReader(self.file_in, delimiter='\t')
+        self.file_in = open(fname, mode='r')
+        self.reader = csv.DictReader(self.file_in, delimiter='\t', quoting=csv.QUOTE_NONE)
         self.data.append([])
         simulation_counter = len(self.data)-1
         self.data[simulation_counter] = []
         for row in self.reader:
             self.data[simulation_counter] += [row]
-#        fields = self.reader.fieldnames
-#        data = {}
-#        for line in self.reader:
-#            for field in fields:
-#                d = data.get(field)
-#                if not d:
-#                    try:
-#                        data[field] = [float(line[field])]
-#                    except:
-#                        data[field] = [str(line[field])]
-#                else:
-#                    try:
-#                        data[field].append(float(line[field]))
-#                    except:
-#                        data[field].append(str(line[field]))
-        #self.data += [buffer]
-        #self.simulation_counter += 1
-        #print buffer
 
     def load_all(self, pattern):
+        self.pattern = pattern
         self.files = glob.glob(os.path.join(self.working_dir, pattern+'*.csv'))
         self.files = sorted(self.files)
         for file in self.files:
             self.load_file(file)
         self.remap_data()
         self.summarize_data()
+        return self.best_case, self.worst_case, self.average_case
         print 'ok'
 
-    def csv_write(self):
-        f = self.filtered_data.keys()
-        print f
-        self.fname = 'eggs.csv'
+    def csv_write_summary(self, fname, fields, summarized_data):
+        self.fname = fname
         self.file_out = open(self.fname, mode='w')
-        self.writer = csv.DictWriter(self.file_out, fieldnames=f, delimiter='\t')
+        self.writer = csv.DictWriter(self.file_out, fieldnames=fields, delimiter='\t', quoting=csv.QUOTE_NONE)
+        
+        headers = dict((n,n) for n in fields)
+        self.writer.writerow(headers)
+        for scenario in summarized_data:
+            d = dict((k, str(v)) for k, v in scenario.items())
+            self.writer.writerow(d)
+        self.file_out.close()
 
-        for column in self.filtered_data.keys():
-            inverted_item = {}
-            for value in self.filtered_data[column]:
-                inverted_item[column] = value
-                print inverted_item
-                self.writer.writerow(inverted_item)
+    def csv_write(self):
+        fields = self.best_case[0].keys()
+        
+        data = self.best_case
+        fname = os.path.join(self.working_dir, self.pattern + '-best.csv')
+        self.csv_write_summary(fname, fields, data)
+        
+        data = self.worst_case
+        fname = os.path.join(self.working_dir, self.pattern + '-worst.csv')
+        self.csv_write_summary(fname, fields, data)
+        
+        data = self.average_case
+        fname = os.path.join(self.working_dir, self.pattern + '-average.csv')
+        self.csv_write_summary(fname, fields, data)
